0ce94fbfb9118cd8214b4eb760da689d33d5ee55
Antoine <marras.antoine@gmail.com>
2020-03-13 01:02:39 +0100

    events added

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import telebot
 import threading
 import time
 import datetime
+
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+from matplotlib.dates import DateFormatter, SECONDLY, rrulewrapper, RRuleLocator, MINUTELY
 
 # custom
 from metricsPooler import MetricsPooler
@@ -64,28 +66,56 @@ def get_logs(message):
         print(str(e))
         bot.reply_to(message, "impossible to get the logs from " + current_day)
 
+@bot.message_handler(commands = ["mark", "stamp", "event"])
+def save_event(message):
+    if mp.save_event():
+        bot.reply_to(message, "event saved in logs")
+    else:
+        bot.reply_to(message, "fail to save the event!")
+
 def send_graph(logs):
     plt.clf()
+
+    fig, ax = plt.subplots()
+
     dico = {}
+    events = []
     lignes = logs.split("\n")
+
     for ligne in lignes:
         try:
             tab = ligne.split()
-            dico[tab[0]] = tab[1]
+            if tab[1] == "event":
+                events.append(tab[0])
+            else:
+                dico[tab[0]] = tab[1]
         except: pass
 
     X = list(dico)
-    Y = [float(dico[k]) for k in X]
-    my_xticks = list(dico)
-    plt.plot(X, Y)
+    X = [datetime.datetime.strptime(x,"%H:%M:%S") for x in X]
+    Y = [float(dico[elem]) for elem in dico]
+    
+    plt.plot_date(X, Y)
+    
+    #plt.set_major_formatter(formatter)
+    print(events, "event")
+    for event in events:
+        print(event)
+        ax.axvline(x=  datetime.datetime.strptime(event, "%H:%M:%S"), color = "red")
+
+    
+
 
     
     step = len(X)//mp.max_number_timestamp_displayed
     step = max(1, step) # to avoid a 0 step if there is not enought measures
 
-    plt.xticks(X[::step], my_xticks[::step])
-    plt.tick_params(axis="x", labelrotation=-60, labelsize = 12)
+    plt.xticks(ticks = X[::step])
+    ax.xaxis.set_tick_params(labelrotation=-60, labelsize = 12)
+    formatter = DateFormatter("%H:%M:%S")
+    ax.xaxis.set_major_formatter(formatter)
     plt.tight_layout()
+
     plt.savefig("graph/graph.png")
     photo = open("graph/graph.png", "rb")
     bot.send_photo(mp.bot_chatID, photo)
@@ -102,12 +132,14 @@ def get_graph(message, current_day = None):
     if logs is not None:
         try:
             send_graph(logs)
-        except:
-            bot.reply_to(message, "unable to retrieve the logs from" + current_day)
+        except Exception as e:
+            bot.reply_to(message, "unable to retrieve the logs from" + current_day + str(e))
     else:
         bot.reply_to(message, "unable to retrieve the logs from" + current_day)
 
 
+
+
 # Launching the background thread ------------------------------------------------------------------------------------------------------------------------
-mp.metricsPolling()
-bot.polling(none_stop=False, interval=2, timeout=20)
+mp.metricsPolling()  # Metric pooler, creation of logs...
+bot.polling(none_stop=False, interval=2, timeout=20)  # telebot bot. 
